3c0e59fafaa3f59f766167e1869af156591c2ec7
Lia Stratopoulos <167905060+lia-viam@users.noreply.github.com>
2026-03-10 16:59:36 -0400

    omitempty distro

--- cli/module_registry.go
+++ cli/module_registry.go
@@ -90,7 +90,7 @@ type manifestBuildInfo struct {
 	Setup      string   `json:"setup"`
 	Path       string   `json:"path"`
 	Arch       []string `json:"arch"`
-	Distro     string   `json:"distro"`
+	Distro     string   `json:"distro,omitempty"`
 	DarwinDeps []string `json:"darwin_deps,omitempty"`
 }
 
